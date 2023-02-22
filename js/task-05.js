f3dc1e7fb8fd3fbff29549f8bdd208480fb302f7
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-02-22 22:52:51 +0100

    cleared task-05

--- js/task-05.js
+++ js/task-05.js
@@ -1,9 +1,6 @@
 const nameIntputEl = document.querySelector(`#name-input`);
 const nameOutputEl = document.querySelector(`#name-output`);
 
-const inputText1 = () =>
-    nameIntputEl.textContent === " " ? nameOutputEl.textContent = `Anonymous` : nameOutputEl.textContent = nameIntputEl.textContent;
-
 const inputText = () =>
     nameIntputEl.textContent === " " ? nameOutputEl.textContent =`Anonymous` : nameOutputEl.textContent = nameIntputEl.value;
 
