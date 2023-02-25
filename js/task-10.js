d8db5aa209ed776c8332e39ba00c0e22c573b688
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-02-25 21:54:32 +0100

    ended task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createBtnEl = document.querySelector(`button[data-create]`);
+const destroyBtnEl = document.querySelector(`button[data-destroy]`);
+const divBoxesEl = document.querySelector(`#boxes`)
+const divControlsEl = document.querySelector(`#controls`)
+
+createBtnEl.addEventListener(`click`, createBoxes)
+destroyBtnEl.addEventListener(`click`, destroyBoxes)
+
+function destroyBoxes() {
+  divBoxesEl.innerHTML = ``;
+  divControlsEl.firstElementChild.value = '';
+}
+
+function createBoxes() {  
+  const quantity = divControlsEl.firstElementChild.value;
+  const collectionBoxes = []
+  
+  let baseHeight = `30`;
+  let baseWidth = `30`;
+  
+  for (let i = 0; i < quantity; i += 1) {
+    const box = document.createElement(`div`);
+    box.style.height = `${baseHeight}px`;
+    box.style.width = `${baseWidth}px`;
+    baseHeight = Number(baseHeight) + 10;
+    baseWidth = Number(baseWidth) + 10;
+    box.style.backgroundColor = `${getRandomHexColor()}`;
+    collectionBoxes.push(box)
+  }
+  divBoxesEl.append(...collectionBoxes)
+}
