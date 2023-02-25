6e0faedac04f6f71e8d54cf46b146e9bee1fafe5
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-02-25 22:20:56 +0100

    update task-10 and task-07

--- js/task-07.js
+++ js/task-07.js
@@ -3,6 +3,11 @@ const textEl = document.querySelector(`#text`);
 
 fontSizeConstrolEl.addEventListener(`input`, handlerfontSizeConstrol)
 
+
+// спеціально додав 8й і 9й рядки, щоб при входи на сторнку текс одразу відповідав розміру на бігунку. Бо без цих 2х рядків виходить неточність, ніби бігунок стоїть на половині своєї стежки, а текс висвітлює якби був на min
+const fontSize = fontSizeConstrolEl.value;
+textEl.style.fontSize = `${fontSize}px`;
+    
 function handlerfontSizeConstrol(event) {
     const fontSize = fontSizeConstrolEl.value;
     textEl.style.fontSize = `${fontSize}px`;
--- js/task-10.js
+++ js/task-10.js
@@ -7,6 +7,10 @@ const destroyBtnEl = document.querySelector(`button[data-destroy]`);
 const divBoxesEl = document.querySelector(`#boxes`)
 const divControlsEl = document.querySelector(`#controls`)
 
+divBoxesEl.style.display = `flex`;
+divBoxesEl.style.flexWrap = `wrap`;
+divBoxesEl.style.gap = `10px`
+
 createBtnEl.addEventListener(`click`, createBoxes)
 destroyBtnEl.addEventListener(`click`, destroyBoxes)
 
@@ -23,12 +27,15 @@ function createBoxes() {
   let baseWidth = `30`;
   
   for (let i = 0; i < quantity; i += 1) {
+    const color = getRandomHexColor();
     const box = document.createElement(`div`);
     box.style.height = `${baseHeight}px`;
     box.style.width = `${baseWidth}px`;
+    box.style.borderRadius = `10%`
+    box.style.boxShadow = `0 0 15px ${color}`
     baseHeight = Number(baseHeight) + 10;
     baseWidth = Number(baseWidth) + 10;
-    box.style.backgroundColor = `${getRandomHexColor()}`;
+    box.style.backgroundColor = `${color}`;
     collectionBoxes.push(box)
   }
   divBoxesEl.append(...collectionBoxes)
